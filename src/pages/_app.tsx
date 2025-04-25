5ca37a6ce3fe850f1f30823c2c64469bf948d239
Ivan Marić <ivan@goodcode.dev>
2025-04-25 15:30:44 +0200

    Update hero

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Montserrat, Source_Serif_4 } from 'next/font/google';
+
+const montserrat = Montserrat({ subsets: ['latin'] });
+const serif = Source_Serif_4({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={montserrat.className}>
+      <div className={serif.className}>
+        <Component {...pageProps} />
+      </div>
+    </main>
+  );
 }
